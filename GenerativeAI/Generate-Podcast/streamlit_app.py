0a860eba206a3d7d8210cd9b70bf55d5da343f02
Suman Das <sumandas@SUMANs-MacBook-Pro-2.local>
2023-05-28 20:37:07 +0530

    Add AI Podcast app

--- GenerativeAI/Generate-Podcast/streamlit_app.py
+++ GenerativeAI/Generate-Podcast/streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import wikipedia
 import tiktoken
 import nltk
+import openai
 
 nltk.download('punkt')
 from nltk.tokenize import sent_tokenize
@@ -31,7 +32,37 @@ def create_chunks(split_sents, max_token_len=2500):
     return input_chunks
 
 
-input_text = st.text_input(label="Enter Wikipedia URL",value="https://en.wikipedia.org/wiki/Lionel_Messi")
+def get_instruct_prompt(topic):
+    return f"""
+    You are a {topic} enthusiast who is doing a research for a podcast. Your task is to extract relevant information from the Result delimited by triple quotes. 
+    Please identify 2 interesting questions and answers which can be used for a podcast discussion.
+    The identified discussions should be returned in thr following format.
+    - Highlight 1 from the text
+    - Highlight 2 from the text
+    """
+
+
+def get_chat_outputs(requestMessages):
+    chatOutputs = []
+    for request in requestMessages:
+        chatOutput = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                  messages=[
+                                                      {"role": "system", "content": "You are a helpful assistant."},
+                                                      {"role": "user", "content": request}
+                                                  ]
+                                                  )
+        chatOutputs.append(chatOutput)
+    return chatOutputs
+
+
+def get_podcast_facts(chatOutputs):
+    podcastFacts = ""
+    for chats in chatOutputs:
+        podcastFacts = podcastFacts + chats.choices[0].message.content
+    return podcastFacts
+
+
+input_text = st.text_input(label="Enter Wikipedia URL", value="https://en.wikipedia.org/wiki/Lionel_Messi")
 
 if not input_text:
     st.error("Valid Wikipedia URL needs to be provided")
@@ -42,7 +73,7 @@ if input_text:
     if not wikipedia_id:
         st.error("Valid Wikipedia URL needs to be provided")
 
-    openai_key = st.text_input(label="Enter OpenAI Key",type="password")
+    openai_key = st.text_input(label="Enter OpenAI Key", type="password")
 
     if wikipedia_id:
         enc = tiktoken.encoding_for_model("gpt-3.5-turbo")
@@ -50,7 +81,18 @@ if input_text:
         wiki_input = input.content
         split_sents = split_text(wiki_input)
         input_chunks = create_chunks(split_sents, max_token_len=2000)
-        st.text_area(label="", value=input_chunks[0], height=100)
+
 
     if not openai_key:
         st.error("OpenAI key needs to be provided!")
+    else:
+        openai.api_key = openai_key
+        topic_name = st.text_input(label="Enter Podcast Topic Name", value="Sports")
+        instructPrompt = get_instruct_prompt(topic_name)
+        requestMessages = []
+        for text in input_chunks:
+            requestMessage = instructPrompt + f"Result: ```{text}```"
+            requestMessages.append(requestMessage)
+        chatOutputs = get_chat_outputs(requestMessages)
+        podcast_facts = get_podcast_facts(chatOutputs)
+        st.text_area(label="", value=podcast_facts, height=200)
